daa4e7902655a35fbb69b2716437001643e4d57d
Julian Florez <forezjuli08@hotmail.com>
2024-07-29 19:23:30 -0500

    update - first step for testing greeting function with various languages

--- main_internal_test.go
+++ main_internal_test.go
@@ -11,10 +11,36 @@ func ExampleMain() {
 // ? A: preparation phase
 // ? B: execution phase
 // ? C: decision phase
-func TestGreet(t *testing.T) {
-	want := "Hello world" //? #A
+func TestGreet_English(t *testing.T) {
+	lang := language("en") //? #A
+	want := "Hello world"  //? #A
 
-	got := greet() //? #B
+	got := greet(lang) //? #B
+
+	if got != want { //? #C
+		// mark this test as failed
+		t.Errorf("expected %s, got %s", want, got)
+	}
+}
+
+func TestGreet_French(t *testing.T) {
+	lang := language("fr")     //? #A
+	want := "Bonjour le monde" //? #A
+
+	got := greet(lang) //? #B
+
+	if got != want { //? #C
+		// mark this test as failed
+		t.Errorf("expected %s, got %s", want, got)
+	}
+}
+
+// Akkadian is not implemented yet!
+func TestGreet_Akkadian(t *testing.T) {
+	lang := language("akk") //? #A
+	want := ""              //? #A
+
+	got := greet(lang) //? #B
 
 	if got != want { //? #C
 		// mark this test as failed
