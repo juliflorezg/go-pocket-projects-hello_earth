7129132306ba33c842b92fc81d6641b46356972d
Julian Florez <forezjuli08@hotmail.com>
2024-07-29 19:07:14 -0500

    add - test to greeting function (use of testing package)

--- main_internal_test.go
+++ main_internal_test.go
@@ -1,7 +1,23 @@
 package main
 
+import "testing"
+
 func ExampleMain() {
 	main()
 	// Output:
 	// Hello world
+}
+
+// ? A: preparation phase
+// ? B: execution phase
+// ? C: decision phase
+func TestGreet(t *testing.T) {
+	want := "Hello world" //? #A
+
+	got := greet() //? #B
+
+	if got != want { //? #C
+		// mark this test as failed
+		t.Errorf("expected %s, got %s", want, got)
+	}
 }
