92d1fe07cc1b495f05c6fce529e25d03036b2faa
Julian Florez <forezjuli08@hotmail.com>
2024-07-29 19:06:54 -0500

    update - extract greeting to another function

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
+	// fmt.Println("Hello world")
+	greeting := greet()
+	fmt.Println(greeting)
+}
+
+func greet() string {
+	return "Hello world"
 }
